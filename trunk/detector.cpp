8b752b526b13e4935bd097178abfe0a4c1e7278b
hrvthzs <hrvthzs@efb84efa-17c2-d7bf-37da-2db28e60f45d>
2011-11-15 18:25:49 +0000

    harris detector, not sure it's correct

--- trunk/detector.cpp
+++ trunk/detector.cpp
@@ -6,8 +6,24 @@
 #include <iostream>
 #include <sstream>
 
+/**
+ *
+ * http://en.wikipedia.org/wiki/Harris_affine_region_detector
+ *
+ */
+
+// Empirical constant 0.04-0.06
+#define K 0.06
+
 using namespace std;
 using namespace cv;
+
+float kernelW(int x, int y, int sigma) {
+    int sigmaSqr = sigma * sigma;
+    int xSqr = x * x;
+    int ySqr = y * y;
+    return pow(M_E, -((xSqr + ySqr) / (2 * sigmaSqr))) / (2 * M_PI * sigmaSqr);
+}
 
 int main(int argc, char* argv[]) {
 
@@ -21,13 +37,15 @@ int main(int argc, char* argv[]) {
             cout << "Use: " << argv[0] << "  -ii inputImageName" << endl;
             return EXIT_SUCCESS;
         } else {
-            cerr << "Error: Unrecognized command line parameter \"" << argv[ i] << "\" use -h to get more information." << endl;
+            cerr << "Error: Unrecognized command line parameter \""
+                 << argv[ i] << "\" use -h to get more information." << endl;
         }
     }
 
 
     if( inputImageName.empty()){
-        cerr << "Error: Some mandatory command line options were not specified." << endl;
+        cerr << "Error: Some mandatory command line options were not specified."
+             << endl;
         return EXIT_FAILURE;
     }
 
@@ -37,25 +55,75 @@ int main(int argc, char* argv[]) {
     imshow("Img", inputImage);
     waitKey();
 
-    Mat verticalGradient(inputImage.rows, inputImage.cols, inputImage.depth());
-    Mat horizontalGradient(inputImage.rows, inputImage.cols, inputImage.depth());
+    Mat Ix(inputImage.rows, inputImage.cols, inputImage.depth());
+    Mat Iy(inputImage.rows, inputImage.cols, inputImage.depth());
     Mat outputImage(inputImage.rows, inputImage.cols, inputImage.depth());
+    Mat cornerImage(inputImage.rows, inputImage.cols, inputImage.depth());
 
-    Sobel(inputImage, verticalGradient, inputImage.depth(), 1, 0, 3);
-    Sobel(inputImage, horizontalGradient, inputImage.depth(), 0, 1, 3);
+
+    ////////////////////////////////////////////////////////////////////////////
+    // SOBEL
+    ////////////////////////////////////////////////////////////////////////////
+    Sobel(inputImage, Iy, inputImage.depth(), 1, 0, 3);
+    Sobel(inputImage, Ix, inputImage.depth(), 0, 1, 3);
 
     for (int i=0; i<inputImage.rows; i++) {
         for (int j=0; j<inputImage.cols; j++) {
-            outputImage.at<char>(i,j) =
-                round(
-                    fabs(verticalGradient.at<char>(i,j)) +
-                    fabs(horizontalGradient.at<char>(i, j))
-                );
-
+            outputImage.at<uchar>(i,j) =
+                round(fabs(Ix.at<uchar>(i,j)) + fabs(Iy.at<uchar>(i, j)));
         }
     }
 
+
     imshow("Img", outputImage);
+    waitKey();
+
+    ////////////////////////////////////////////////////////////////////////////
+    // HARRIS CORNER DETECTOR
+    ////////////////////////////////////////////////////////////////////////////
+    int counter = 0;
+    Mat movementMatrix(4,4, CV_32F);
+    for (int i=0; i<inputImage.rows; i++) {
+        for (int j=0; j<inputImage.cols; j++) {
+            uint x = Ix.at<uchar>(j,i);
+            uint y = Ix.at<uchar>(j,i);
+            uint xx = x * x;
+            uint yy = y * y;
+            uint xy = x * y;
+
+            float kernelSum = 0.0f;
+
+            for (int m=0; m<5; m++) {
+                for (int n=0; n<5; n++) {
+                    kernelSum = kernelW(m,n, 5.0);
+                }
+            }
+
+            movementMatrix.at<float>(0,0) = xx * kernelSum;
+            movementMatrix.at<float>(0,1) = xy * kernelSum;
+            movementMatrix.at<float>(1,0) = xy * kernelSum;
+            movementMatrix.at<float>(1,1) = yy * kernelSum;
+
+            float det = determinant(movementMatrix);
+            Vec<float,4> tmp = trace(movementMatrix);
+            float tr = tmp[0];
+
+
+            float R = fabs(det - K * tr * tr);
+
+            if (R > 1000.0f) {
+                //cout << R << endl;
+                counter++;
+                cornerImage.at<uchar>(j,i) = inputImage.at<uchar>(j,i);
+
+            }
+        }
+    }
+
+    cout << counter << endl;
+
+    imshow("Img1", inputImage);
+    imshow("Img2", cornerImage);
     waitKey();
 
     return EXIT_SUCCESS;
