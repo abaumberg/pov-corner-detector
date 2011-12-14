98bfbf7b4c6837cff23e607dbe878fb45b9d55bc
semmineku <semmineku@efb84efa-17c2-d7bf-37da-2db28e60f45d>
2011-12-14 14:06:56 +0000

    harrislaplace

--- trunk/detector.cpp
+++ trunk/detector.cpp
@@ -29,12 +29,13 @@
 #define LAPLACE_S_START 0.7
 #define LAPLACE_S_COUNT 8
 
-//double const Pi=4*atan(1);
-
 using namespace std;
 using namespace cv;
 
-
+struct ScaledPoint {
+	float scale;
+	Point point;
+};
 
 float gaussianDerivate(int x, int y, float sigma) {
 	return (-x * pow(M_E, (-(x*x+y*y)/(2*sigma*sigma)) ))/(2* M_PI * pow(sigma,4));
@@ -53,59 +54,6 @@ Mat gaussianDerivateKernel(float sigma, bool direction = 0) {
 	}
 
 	return kernel;
-}
-
-void detectCorners(Mat &src, Mat &dst, float sigmaI, float sigmaD) {
-
-    Mat Ix, Iy, Ixx;
-
-    //Sobel(src, Ix, CV_32F, 1, 0, kernelSize);
-    //Sobel(src, Iy, CV_32F, 0, 1, kernelSize);
-
-    filter2D(src, Ix, CV_32F, gaussianDerivateKernel(sigmaD));
-    filter2D(src, Iy, CV_32F, gaussianDerivateKernel(sigmaD,1));
-
-    Size size = src.size();
-    Mat covariance( size, CV_32FC3 );
-
-    for(int i=0; i<covariance.rows; i++ ) {
-        for(int j = 0; j<covariance.cols; j++ ) {
-
-            float x = Ix.at<float>(i,j);
-            float y = Iy.at<float>(i,j);
-
-            Vec3f v;
-            v[0] = x*x;
-            v[1] = x*y;
-            v[2] = y*y;
-
-            covariance.at<Vec3f>(i,j) = v;
-        }
-    }
-
-    int kernelSize = ceil(sigmaI) * 2 + 1;
-    Mat gaussKernel = getGaussianKernel(kernelSize, sigmaI);
-    filter2D(covariance, covariance, covariance.depth(), gaussKernel);
-    //boxFilter(covariance, covariance, covariance.depth(), Size(blockSize, blockSize));
-
-    if(covariance.isContinuous() && covariance.isContinuous()) {
-        size.width *= size.height;
-        size.height = 1;
-    }
-
-    for(int i=0 ; i < covariance.rows; i++ ) {
-        for(int j = 0; j < covariance.cols; j++ ){
-
-            Vec3f v = covariance.at<Vec3f>(i,j);
-            v *= sigmaD * sigmaD;
-            float a = v[0];
-            float b = v[1];
-            float c = v[2];
-            dst.at<float>(i,j) = (float)(a*c - b*b - K*(a + c)*(a + c));
-        }
-    }
-
-
 }
 
 void harrisDetector(Mat &src, Mat &dst, float sigmaI, float *maxR) {
@@ -187,6 +135,107 @@ float getLaplacian(Mat &src, int x, int y, float sigma0, float *maxR) {
     return max;
 }
 
+void getHarrisLaplace(Mat &src, float sigma0, map<int, Point> harrisPoints, map<int, ScaledPoint> &harrisLaplacePoints) {
+	Mat DefIxx[LAPLACE_S_COUNT], DefIyy[LAPLACE_S_COUNT];
+	
+    float sigma;
+	for (int i = 0; i < LAPLACE_S_COUNT; i++) {
+		sigma = sigma0 * (LAPLACE_S_START + LAPLACE_S_STEP * i);
+		
+		filter2D(src, DefIxx[i], CV_32F, gaussianDerivateKernel(sigma));
+		filter2D(DefIxx[i], DefIxx[i], CV_32F, gaussianDerivateKernel(sigma));
+		filter2D(src, DefIyy[i], CV_32F, gaussianDerivateKernel(sigma, 1));
+		filter2D(DefIyy[i], DefIyy[i], CV_32F, gaussianDerivateKernel(sigma, 1));
+	}
+	
+    map<int,Point>::iterator it;
+
+    for (it = harrisPoints.begin(); it != harrisPoints.end(); ++it) {
+		int xK0 = (it->second).x;
+		int yK0 = (it->second).y;
+		bool condition = true;
+		int xK1 = xK0, yK1 = yK0, iK;
+		float sigmaK0 = sigma0, sigmaK1;
+		float curLoG, curSigma;
+		float LoG = FLT_MIN;
+		Mat Ixx, Iyy;
+		Mat curIxx(DefIxx[0].size(), DefIxx[0].type());
+		Mat curIyy(DefIyy[0].size(), DefIyy[0].type());
+		
+		for (int i = 0; i < LAPLACE_S_COUNT; i++) {
+			curSigma = sigma0 * (LAPLACE_S_START + LAPLACE_S_STEP * i);
+			
+			curLoG = curSigma * curSigma * abs(DefIxx[i].at<float>(yK0,xK0) + DefIyy[i].at<float>(yK0,xK0));
+			if (LoG < curLoG) {
+				LoG = curLoG;
+				sigmaK1 = curSigma;
+				iK = i;
+			}
+		}
+		
+		if (LoG < THRESHOLD_LAPLACE) {
+			continue;
+		}
+		
+		
+		for (int m=-1; m<=1; m++) {
+			for (int n=-1; n<=1; n++) {
+				if (n == 0 && m == 0) continue;
+
+				curLoG = sigmaK1 * sigmaK1 * abs(DefIxx[iK].at<float>(yK0+m,xK0+n) + DefIyy[iK].at<float>(yK0+m,xK0+n));
+
+				if (LoG < curLoG) {
+					LoG = curLoG;
+					xK1 = xK0+n;
+					yK1 = yK0+m;
+				}
+			}
+		}
+		
+		while (xK1 != xK0 || yK1 != yK0 || sigmaK0 != sigmaK1) {
+			cout << "X[" << xK1 << "," << yK1 << "]: LoG " << LoG << ", Sigma " << sigmaK1 << endl;
+			xK0 = xK1;
+			yK0 = yK1;
+			sigmaK0 = sigmaK1;
+		
+			for (int i = 0; i < LAPLACE_S_COUNT; i++) {
+				curSigma = sigmaK0 * (LAPLACE_S_START + LAPLACE_S_STEP * i);
+			
+				filter2D(src, Ixx, CV_32F, gaussianDerivateKernel(curSigma));
+				filter2D(Ixx, Ixx, CV_32F, gaussianDerivateKernel(curSigma));
+				filter2D(src, Iyy, CV_32F, gaussianDerivateKernel(curSigma, 1));
+				filter2D(Iyy, Iyy, CV_32F, gaussianDerivateKernel(curSigma, 1));
+				
+				curLoG = curSigma * curSigma * abs(Ixx.at<float>(yK0,xK0) + Iyy.at<float>(yK0,xK0));
+				if (LoG < curLoG) {
+					LoG = curLoG;
+					sigmaK1 = curSigma;
+					curIxx = Ixx.clone();
+					curIyy = Iyy.clone();
+				}
+			}
+			
+			for (int m=-1; m<=1; m++) {
+				for (int n=-1; n<=1; n++) {
+					if (n == 0 && m == 0) continue;
+
+					curLoG = sigmaK1 * sigmaK1 * abs(curIxx.at<float>(yK0+m,xK0+n) + curIyy.at<float>(yK0+m,xK0+n));
+
+					if (LoG < curLoG) {
+						LoG = curLoG;
+						xK1 = xK0+n;
+						yK1 = yK0+m;
+					}
+				}
+			}
+		}
+		
+		harrisLaplacePoints[xK1 + curIxx.rows * yK1].scale = sigmaK1;
+		harrisLaplacePoints[xK1 + curIxx.rows * yK1].point = Point(xK1, yK1);
+		cout << "O[" << xK1 << "," << yK1 << "]: LoG " << LoG << ", Sigma " << sigmaK1 << endl;
+    }
+}
+
 int main(int argc, char* argv[]) {
 
     /*map<float, map<int, Point> > points;
@@ -246,11 +295,16 @@ int main(int argc, char* argv[]) {
     Mat corners = Mat::zeros( grayImage.size(), CV_32FC1 );
     /// Detector parameters
 
+
+	map<float, map<int, Point> > harrisPoints;
+	map<int, ScaledPoint> harrisLaplacePoints;
 	Mat outputImage = inputImage.clone();
 	float max;
-	float s = 1.5;
+	float s = S0;
 	int countdown = COUNTDOWN;
     do {
+		harrisPoints[s] = map<int, Point>();
+		
 		harrisDetector(grayImage, dst, s, &max);
 		//normalize( dst, dst_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat() );
 		//convertScaleAbs( dst_norm, dst_norm_scaled );
@@ -262,7 +316,6 @@ int main(int argc, char* argv[]) {
 					float cur_point = (float) dst.at<float>(j,i);
 
 					if (cur_point > THRESHOLD_HARRIS) {
-						//circle(outputImage, Point(i,j), 5,  Scalar(255, 0, 0), 1, 8, 0 );
 						counter++;
 
 						bool condition = true;
@@ -286,20 +339,25 @@ int main(int argc, char* argv[]) {
 						if (condition) {
 							counter2++;
 							cornerScales.at<float>(j,i) = s;
+							harrisPoints[s][i + dst.rows * j] = Point(i, j);
+							
 
-							for (int m=-1; m<=1; m++) {
-								for (int n=-1; n<=1; n++) {
-									if (n == 0 && m == 0) continue;
+							//for (int m=-1; m<=1; m++) {
+							//	for (int n=-1; n<=1; n++) {
+							//		if (n == 0 && m == 0) continue;
 
 									//cornerScales.at<float>(j+m,i+n) = 0;
-								}
-							}
+							//	}
+							//}
 						}
 					}
 				}
 			}
 			cout << "Corners unfiltered: " << counter << endl;
 			cout << "Corners filtered: " << counter2 << endl;
+
+		getHarrisLaplace(grayImage, s, harrisPoints[s], harrisLaplacePoints);
+		
 		s *= epsilon;
 		if (max > THRESHOLD_HARRIS) {
 			countdown = COUNTDOWN;
@@ -308,8 +366,15 @@ int main(int argc, char* argv[]) {
 		}
 		cout << max << endl;
 	} while (countdown > 0);
+	
+	
+    map<int,ScaledPoint>::iterator it;
 
-		float Laplaci;
+    for(it = harrisLaplacePoints.begin(); it != harrisLaplacePoints.end(); ++it) {
+		circle(outputImage, (it->second).point, 3*(it->second).scale,  Scalar(255-(int)3*3*(it->second).scale, 0, (int)3*3*(it->second).scale), 2, 8, 0 );
+    }
+
+	/*	float Laplaci;
 		float sigmaX;
 	for( int j = 0; j < cornerScales.rows ; j++ ) {
 		for( int i = 0; i < cornerScales.cols; i++ ) {
@@ -322,7 +387,7 @@ int main(int argc, char* argv[]) {
 				}
 			}
 		}
-	}
+	}*/
 
 	namedWindow("Corners", CV_WINDOW_NORMAL);
 	imshow("Corners", outputImage );
